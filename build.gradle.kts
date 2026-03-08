0d9d19fe3b1a9170ce99449df307102b93325a84
Hallvard Ystad <hallvard.ystad@babelserver.org>
2026-03-08 22:31:38 +0100

    Add github workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "2.3.10" // https://plugins.gradle.org/plugin/org.jetbrains.kotlin.jvm
     id("org.jetbrains.intellij.platform") version "2.12.0" // https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html#usage
+    id("org.jetbrains.kotlinx.kover") version "0.9.1" // https://github.com/Kotlin/kotlinx-kover
     id("org.babelserver.gradle.test-logger") version "2.0.0"
 }
 
