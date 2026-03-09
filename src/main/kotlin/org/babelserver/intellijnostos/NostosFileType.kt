d90bab4159c0d7783b2a1b425cf9a6cfbac80749
Hallvard Ystad <hallvard.ystad@babelserver.org>
2026-03-09 14:07:04 +0100

    Add some icons

--- src/main/kotlin/org/babelserver/intellijnostos/NostosFileType.kt
+++ src/main/kotlin/org/babelserver/intellijnostos/NostosFileType.kt
@@ -1,11 +1,12 @@
 package org.babelserver.intellijnostos
 
 import com.intellij.openapi.fileTypes.LanguageFileType
+import com.intellij.openapi.util.IconLoader
 import javax.swing.Icon
 
 object NostosFileType : LanguageFileType(NostosLanguage) {
     override fun getName(): String = "Nostos"
     override fun getDescription(): String = "Nostos language file"
     override fun getDefaultExtension(): String = "nos"
-    override fun getIcon(): Icon? = null
+    override fun getIcon(): Icon = IconLoader.getIcon("/icons/nostos.svg", NostosFileType::class.java)
 }
